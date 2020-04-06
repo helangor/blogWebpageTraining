1c75c9519eb9248664afb45606a1bcd099cb0d66
helangor <henrikki_helander@hotmail.com>
2020-04-06 15:19:31 +0300

    added contact and about pages

--- app.js
+++ app.js
@@ -20,6 +20,16 @@ app.get("/", function(req, res) {
   startingContent: homeStartingContent});
 });
 
+app.get("/contact", function(req, res) {
+  res.render("contact", {
+  contactContent: contactContent});
+});
+
+app.get("/about", function(req, res) {
+  res.render("about", {
+  aboutContent: aboutContent});
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
