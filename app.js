b2a71dbe17defdc6c12d4d639275264c5998214b
helangor <henrikki_helander@hotmail.com>
2020-04-06 20:03:07 +0300

    Added saving title and content

--- app.js
+++ app.js
@@ -15,9 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function(req, res) {
   res.render("home", {
   startingContent: homeStartingContent});
+  console.log(posts);
+
 });
 
 app.get("/contact", function(req, res) {
@@ -35,8 +39,12 @@ app.get("/compose", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const item = req.body.postTitle;
-  console.log(item);
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+posts.push(post);
+res.redirect("/");
 });
 
 app.listen(3000, function() {
